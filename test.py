61ffc0b0544bbce8d6c0c556b058264e0b5990fe
heeeeoeeeeol <60788888+heeeeoeeeeol@users.noreply.github.com>
2023-09-01 04:55:31 -0700

    Update test.py
    - updated to test using tester() class function

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from AES import AES
 
+
+# space test
 key = "2B 7 E15  1 628  A ED2  A 6 A B  F7 15 8809CF4F3C"
 text = "32 43 f 6 a 8     88 5a3  08d 3131 98 a2e0370734"
 test = AES(text, key) 
@@ -38,42 +40,37 @@ for i in rk:
     print(test4.parseWords(test4.toString(i)))
 
 
-# appendix B states array 
+# appendix B intermedite value testing
 print('\n')
 Input = "32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34"
 Cipher_Key = "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c"
 example = AES(Input, Cipher_Key)
 example.Cipher()
-states = example.getStates()
-for i in states:
-    print(example.toString(i))
+example.InvCipher()
+print(example.tester())
 
-# appendix C 
+# appendix C more intermediate value testing
 print('\n')
 PLAINTEXT = "00112233445566778899aabbccddeeff"
 KEY = "000102030405060708090a0b0c0d0e0f" # 128 bit
 test = AES(PLAINTEXT, KEY)
-c = test.Cipher()
-print(test.toString(c))
-invc = test.InvCipher()
-print(test.toString(invc))
+test.Cipher()
+test.InvCipher()
+print(test.tester())
 
 print('\n')
 PLAINTEXT = "00112233445566778899aabbccddeeff"
 KEY = "000102030405060708090a0b0c0d0e0f1011121314151617" # 192 bit 
 test = AES(PLAINTEXT, KEY)
-c = test.Cipher()
-print(test.toString(c))
-invc = test.InvCipher()
-print(test.toString(invc))
+test.Cipher()
+test.InvCipher()
+print(test.tester())
 
 print('\n')
 PLAINTEXT = "00112233445566778899aabbccddeeff"
 KEY = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f" # 256 bit 
 test = AES(PLAINTEXT, KEY)
-c = test.Cipher()
-print(test.toString(c))
-invc = test.InvCipher()
-print(test.toString(invc))
+test.Cipher()
+test.InvCipher()
+print(test.tester())
 
-    
